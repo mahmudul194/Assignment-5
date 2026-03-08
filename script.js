f45281e58b4c0ca6869485c28a21f34092be6d6c
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-09 00:56:34 +0600

    assigned variables for inputs and buttons and other stuffs

--- script.js
+++ script.js
@@ -2,3 +2,37 @@ const API_BASE_URL = 'https://phi-lab-server.vercel.app/api/v1/lab';
 const DEMO_USER = 'admin';
 const DEMO_PASS = 'admin123';
 
+let allIssues = [];
+let currentFilter = 'all';
+
+const loginPage = document.getElementById('login-page');
+const mainPage = document.getElementById('main-page');
+
+const loginForm = document.getElementById('login-form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginError = document.getElementById('login-error');
+
+const issuesContainer = document.getElementById('issues-container');
+const issueCount = document.getElementById('issue-count');
+const tabBtns = document.querySelectorAll('.tab-btn');
+const loadingSpinner = document.getElementById('loading-spinner');
+const emptyState = document.getElementById('empty-state');
+
+const searchInput = document.getElementById('search-input');
+const searchInputMobile = document.getElementById('search-input-mobile');
+
+const modal = document.getElementById('issue-modal');
+const modalLoader = document.getElementById('modal-loader');
+const modalContent = document.getElementById('modal-content');
+const closeBtns = document.querySelectorAll('.close-modal');
+
+const modalTitle = document.getElementById('modal-title');
+const modalStatusBadge = document.getElementById('modal-status-badge');
+const modalAuthor = document.getElementById('modal-author');
+const modalDate = document.getElementById('modal-date');
+const modalLabels = document.getElementById('modal-labels');
+const modalDescription = document.getElementById('modal-description');
+const modalAssignee = document.getElementById('modal-assignee');
+let modalPriorityBadge = document.getElementById('modal-priority-badge');
+
